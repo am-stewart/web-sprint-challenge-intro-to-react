d66c9bed48e8a8445974ecab9bafc45c31235d20
Allison Stewart <amstewart@yahoo.com>
2021-12-10 11:04:30 -0500

    used api to get data and rendered names to character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Character = props => {
+    console.log(props, 'this is props')
+    return (
+        <div>
+           {props.character.name} 
+        </div>
+    )
+}
+
+export default Character;
